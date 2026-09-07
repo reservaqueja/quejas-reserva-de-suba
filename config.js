f91d649c653e3c0e1a341e0247c4a4e1a2551b74
reservaqueja <reservadesubaque@outlook.com>
2026-09-06 20:53:51 -0500

    Fix formatting in whatsappMensaje
    Updated whatsappMensaje to remove extra spaces.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ window.CONJUNTO_CONFIG = {
 
   instructivosUrl: "https://drive.google.com/file/d/1CaF214o654Dcz0FkSd2EUQer54GnZJiO/view?usp=drive_link",
   whatsappPruebas: "573174683541",
-  whatsappMensaje: "Hola, administración Reserva de Suba. Envío pruebas de mi queja. Radicado:      Adjunto el registro visual como prueba de la infracción. Les recuerdo que este material contiene datos sensibles (rostros), por lo cual su uso es de carácter estrictamente reservado y confidencial para el proceso interno, de acuerdo con la Ley 1581 de 2012.",
+  whatsappMensaje: "Hola, administración Reserva de Suba. Envío pruebas de mi queja. Radicado:-----------Adjunto el registro visual como prueba de la infracción. Les recuerdo que este material contiene datos sensibles (rostros), por lo cual su uso es de carácter estrictamente reservado y confidencial para el proceso interno, de acuerdo con la Ley 1581 de 2012.",
 
   web3formsAccessKey: "f56f70d8-4530-453b-a46d-7955102a5f0d",
   envioAutomatico: true,
